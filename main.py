e8500762326398dd34f946591f97b08049481a3d
Igaki12 <igakin8.19971222@gmail.com>
2024-05-17 14:12:51 +0900

    made screen of clinical record for the first time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import eel
 
+
 question_list = [
     {
         "question": "Q1. 症例１",
@@ -11,13 +12,33 @@ question_list = [
         "question": "Q3. 症例３",
     }
 ]
+# import threading
+#１つ目のウィンドウを起動
+# def open_window1():
+#     eel.start("index.html" , block=False , size=(800, 600))
+#     eel.sleep(1)
 
+# #2つ目のウィンドウを起動
+# def open_window2():
+#     eel.start("chat_screen_generated2.html" , block=False , size=(300, 600))
+#     eel.sleep(1)
 
 def main():
     eel.init("docs")
-    eel.start("index.html")
-    # ２つ目のスクリーンを起動
-    eel.start("chat_screen_generated2.html")
+    #２つのスクリーンを同時に起動
+    eel.start("index.html", size=(800, 600))
+
+# def main():
+#     eel.init("docs")
+#     thread1 = threading.Thread(target=open_window1)
+#     thread2 = threading.Thread(target=open_window2)
+#     # スレッドの開始
+#     thread1.start()
+#     thread2.start()
+#     # スレッドの終了を待つ
+#     thread1.join()
+#     thread2.join()
+
 
 
 """link1が押下された際に呼び出すスクリプト"""
